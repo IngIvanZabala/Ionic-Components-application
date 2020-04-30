803ed433fcae006dba80c549567f6fe9e1ce3edd
IngIvanZabala <40217745+IngIvanZabala@users.noreply.github.com>
2020-04-29 20:18:00 -0500

    adding new components

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -6,7 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
-  routes : component[] = [{icon:'american-football',title:"Alert", redirectTo:"/alert"}, {icon:'cart-outline',title:"Action Sheet", redirectTo:"/action-sheet"}]
+  routes : component[] = 
+  [
+    {
+      icon:'american-football',
+      title:"Alert", 
+      redirectTo:"/alert"
+    }, 
+    {
+      icon:'cart-outline',
+      title:"Action Sheet", 
+      redirectTo:"/action-sheet"
+    },
+    {
+      icon:'person-outline',
+      title:'Avatar',
+      redirectTo:'/avatar'
+    },
+    {
+      icon:'apps-outline',
+      title:'Botones',
+      redirectTo:'/button'
+    },
+    {
+      icon:'card-outline',
+      title:'Cards',
+      redirectTo:'/card'
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
